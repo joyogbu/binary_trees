3fef42078e8b7fb00ae4becc777d898f12e4a39e
Joy Ogbu <obyjoy@yahoo.com>
2023-03-03 09:35:45 -0800

    cotrrection on task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,18 +7,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	int left_height = 0;
+	int right_height = 0;
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		left_height = binary_tree_height(tree->left);
+		left_height += 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		right_height = binary_tree_height(tree->right);
+		right_height += 1 + binary_tree_height(tree->right);
 	if (left_height >= right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
 	{
-		return (right_height + 1);
+		return (right_height);
 	}
 }
