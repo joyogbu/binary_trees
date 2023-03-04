83e4ca98eff8a2b4a9f145baa32dfe63b891aa29
Joy Ogbu <obyjoy@yahoo.com>
2023-03-04 03:59:45 -0800

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,4 +13,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->leftmnmñññ	
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+}
